011ba07746e2a35e4a0fa13cce0de61af9fd4e82
tariqcryptoo <tariq.cryptoo@gmail.com>
2025-04-17 11:43:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from telegram import Bot
+from telegram.ext import Updater, MessageHandler, Filters
+
+def get_chat_id(update, context):
+    chat_id = update.effective_chat.id
+    print(f"Chat ID: {chat_id}")
 
 def main():
-    print("Bot is starting...")
-
-    bot = Bot(token="7239933938:AAEhm_lWwAr7JcGomW8-EJa_rg0_BbpczdQ")
-    bot.send_message(chat_id="1273613018", text="البوت اشتغل بنجاح من Render!")
+    updater = Updater("7239933938:AAEhm_lWwAr7JcGomW8-EJa_rg0_BbpczdQ", use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(MessageHandler(Filters.text, get_chat_id))
+    updater.start_polling()
+    updater.idle()
 
 if __name__ == "__main__":
     main()
