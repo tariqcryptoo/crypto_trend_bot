e7fd9920c7ec8393d74da44c1fb79b7437acb6f6
tariqcryptoo <tariq.cryptoo@gmail.com>
2025-04-17 10:45:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,7 @@ from telegram import Bot
 def main():
     print("Bot is starting...")
 
-    # إنشاء البوت
-    bot = Bot(token="هنا_تحط_توكن_البوت")
-
-    # إرسال رسالة إلى حسابك في تيليجرام
+    bot = Bot(token="7239933938:AAEhm_lWwAr7JcGomW8-EJa_rg0_BbpczdQ")
     bot.send_message(chat_id="1273613018", text="البوت اشتغل بنجاح من Render!")
 
 if __name__ == "__main__":
