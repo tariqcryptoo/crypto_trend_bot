55a6332ce40aa18c48df6a8f542096fdad65196f
tariqcryptoo <tariq.cryptoo@gmail.com>
2025-04-17 12:57:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from telegram.ext import Updater, MessageHandler, Filters
 
+BOT_TOKEN = "7239933938:AAEhm_lWwAr7JcGomW8-EJa_rg0_BbpczdQ"
+
 def get_chat_id(update, context):
     chat_id = update.effective_chat.id
     print(f"Chat ID: {chat_id}")
+    context.bot.send_message(chat_id=chat_id, text=f"تم استلام الرسالة! معرف القروب هو:\n{chat_id}")
 
 def main():
-    updater = Updater("7239933938:AAEhm_lWwAr7JcGomW8-EJa_rg0_BbpczdQ", use_context=True)
+    updater = Updater(BOT_TOKEN, use_context=True)
     dp = updater.dispatcher
     dp.add_handler(MessageHandler(Filters.text, get_chat_id))
     updater.start_polling()
